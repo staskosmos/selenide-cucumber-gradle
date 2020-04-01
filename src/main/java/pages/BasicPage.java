24b5975a4694f7140da166afdc351829415c248d
staskosmos <mr.varyn@mail.ru>
2020-04-02 01:06:54 +0300

    add basic page and Hooks

---
+++ src/main/java/pages/BasicPage.java
@@ -0,0 +1,32 @@
+package pages;
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class BasicPage {
+
+    public void clickButton(String text){
+        $(By.xpath("//div[text()='"+text+"']")).click();
+    }
+
+    public void clickButtonSpan(String text){
+        $(By.xpath("//span[text()='"+text+"']/..")).click();
+    }
+
+    public void clickButtonLi(String text){
+        $(By.xpath("//li[text()='"+text+"']/..")).click();
+    }
+
+    public void contentIsVisible(String text){
+        $(By.xpath("//*[text()='"+text+"']")).shouldBe(Condition.visible);
+    }
+
+    public boolean checkTextExists(String text){
+        if ($(By.xpath("//*[text()='"+text+"']")).exists()){
+            return true;
+        }
+        else return false;
+    }
+}
+
