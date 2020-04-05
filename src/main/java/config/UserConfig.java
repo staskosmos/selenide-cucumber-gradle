fc75858e4e2b94c7e8dd522867c3b35ec447189d
staskosmos <mr.varyn@mail.ru>
2020-04-06 00:56:19 +0300

    add user config

---
+++ src/main/java/config/UserConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+public class UserConfig {
+    public static final String USER_LOGIN="test@test.com";
+    public static final String USER_PASSWORD="Password!1";
+    public static final String USER_FIRST_NAME="Auto_First_Name";
+    public static final String USER_LAST_NAME="Auto_Last_Name!1";
+}
