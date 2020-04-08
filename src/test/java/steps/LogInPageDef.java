37e3bd6621088aee15bfc66c3fb1d1ce206a3f02
staskosmos <mr.varyn@mail.ru>
2020-04-08 20:55:21 +0300

    add api methods tests

--- src/test/java/steps/LogInPageDef.java
+++ src/test/java/steps/LogInPageDef.java
@@ -1,16 +1,21 @@
 package steps;
 
 import config.UserConfig;
+import impl.AuthServiceImpl;
 import io.cucumber.java.en.Then;
+import models.api.User;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
+import org.junit.Test;
 import pages.LogInPage;
+import services.AuthService;
 
 import static app_context.RunContext.get;
 import static app_context.RunContext.put;
 
 public class LogInPageDef {
     LogInPage logInPage = new LogInPage();
+    AuthService authService = new AuthServiceImpl();
 
     @Then("Input email")
     public  void inputEmail() {
@@ -51,5 +56,30 @@ public class LogInPageDef {
         String expectLastName = get("lastName",String.class);
         String actualLastName = logInPage.getLastName();
         Assert.assertEquals(expectLastName,actualLastName);
+    }
+
+    @Then("Create user by API")
+    public void createUserByAPI(){
+        User newUser = User.builder()
+                .email("auto_"+RandomStringUtils.randomAlphabetic(5)+"@mail.com")
+                .firstName("TestFirstName")
+                .lastName("TestLastName")
+                .password(UserConfig.USER_PASSWORD)
+                .rememberMe(true)
+                .build();
+
+        put("user",newUser);
+
+        User createdUser = authService.createUser(newUser);
+        Assert.assertNotNull(createdUser);
+        Assert.assertEquals(newUser.getEmail().toLowerCase(), createdUser.getEmail().toLowerCase());
+        Assert.assertNotNull(createdUser.getId());
+    }
+
+    @Then("Login user by API")
+    public void loginUserByAPI() {
+        User user = get("user", User.class);
+        User userLogged = authService.login(user);
+        Assert.assertNotNull(userLogged);
     }
 }
